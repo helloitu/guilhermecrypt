3cd881de34a923472146fb4a9854cc3cb7df48f8
helloitu <gui.gui.guicesar12@hotmail.com>
2015-04-21 10:42:28 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+echo Iniciando Instalação GuilhermeCrypt
+echo ----------------------
+echo ------------------------------
+echo ---------------------------------------
+sudo apt-get install figlet
+figlet B E - W H I T E
+echo --------------------------------
+echo Be - White , follow the Rabbit 
+echo --------------------------------
+sudo cp  guilhermecrypt   /bin/
+cd /bin/
+sudo chmod 777 guilhermecrypt 
+echo ---------------------------------------------
+echo  Instalação Completa , Obrigado por Usar :D
